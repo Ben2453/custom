1e715f1e156ddc6303275fc13926553f83538430
| Ben2453 | <ben2453mc@gmail.com>
2016-06-07 12:41:10 +1000

    Update extension.js

--- extension.js
+++ extension.js
@@ -112,6 +112,6 @@
     }));
 
     // Start the bot and extend it when it has loaded.
-    $.getScript("https://rawgit.com/basicBot/source/master/basicBot.js", extend);
+    $.getScript("https://rawgit.com/Ben2453/source/master/basicBot.js", extend);
 
 }).call(this);
